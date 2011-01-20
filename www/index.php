1498bd13a95e3aa3ed10b09729eb59e53ee6e459
cstrieder ( ) <cstrdr@gmail.com>
2011-01-20 13:50:08 +0000

    test
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/pasmae@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,9 +43,15 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
+<h2>O que é PASMAE?</h2>
+
+<p>Protocolo para Ajuste e Seleção de Modelos de Abundância de Espécies
+</p>
+
+<!-- 
 <p> No content added. </p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
-
+ -->
 </body>
 </html>
